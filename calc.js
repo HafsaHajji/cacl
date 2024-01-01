2101412b26b52529c6b43fd64b01171fe0372cfd
HafsaHajji <xafsaxaaji936@gmail.com>
2024-01-01 11:29:03 +0300

    calculator

---
+++ calc.js
@@ -0,0 +1,36 @@
+let display = document.querySelector('.display');
+let buttons = document.querySelector('.buttons');
+let specialChars = ('%' ,' * ', ' / ' , ' - ', '+ ', '=');
+
+let output = "";
+
+const calculate = (btnValue) =>{
+    if (btnValue ==='=' && output !== ""){
+        output = eval( output.replace("%" , "/100"));
+    }
+
+    else if(btnValue === "AC"){
+        output = "";
+    }
+    else if (btnValue === "DEL"){
+        output = output.toString().slice(0,-1);
+    }
+
+    else{
+        if (output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue;
+    }
+    display.value = output;
+}
+
+
+
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click' , (e)=> {
+        calculate(e.target.dataset.value);
+
+    });
+    
+});
